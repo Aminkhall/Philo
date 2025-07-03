a77eb722cec00e5e14ff01b651820e51d6177b69
Mohamed Amine Khallouk <mkhallou@e1r6p9.1337.ma>
2025-07-03 15:16:25 +0100

    2

--- philo.c
+++ philo.c
@@ -6,27 +6,108 @@
 /*   By: mkhallou <mkhallou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 11:27:41 by mkhallou          #+#    #+#             */
-/*   Updated: 2025/06/30 19:54:00 by mkhallou         ###   ########.fr       */
+/*   Updated: 2025/07/03 15:14:51 by mkhallou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// void ft_creat_philo(t_philo philo)
-// {
-//     int *philosopher;
+void init_main(t_main *data, int num_philo)
+{
+    int i;
+
+    data->forks = malloc(sizeof(t_philo) * num_philo);
+    if (!data->forks)
+        exit_error("Somting wrong with malloc");
+    i = 0;
+    while (i < num_philo)
+    {
+        if (pthread_mutex_init(&data->forks[i], NULL) != 0) {
+            fprintf(stderr, "Failed to initialize mutex\n");
+            return ;
+        }
+        ++i;
+    }
     
-//     pthread_create()
-// }
+    if (pthread_mutex_init(&data->dead_lock, NULL) != 0) {
+        fprintf(stderr, "Failed to initialize mutex\n");
+        return ;
+    }
+    if (pthread_mutex_init(&data->meal_lock, NULL) != 0) {
+        fprintf(stderr, "Failed to initialize mutex\n");
+        return ;
+    }
+    if (pthread_mutex_init(&data->write_lock, NULL) != 0) {
+        fprintf(stderr, "Failed to initialize mutex\n");
+        return ;
+    }
+}
+
+void init_philo(t_main *data, char **av)
+{
+    int i;
+
+    data->philos = malloc(sizeof(t_philo) * ft_atol(av[1]));
+    if (!data->philos)
+        exit_error("Somting wrong with malloc");
+    init_main(data, ft_atol(av[1]));
+    i = 0;
+    while (i < ft_atol(av[1]))
+    {
+        data->philos->id = i + 1;
+        data->philos->meals_eaten = 0;
+        data->philos[i].num_of_philos = ft_atol(av[1]);
+        data->philos[i].time_to_die = ft_atol(av[2]);
+        data->philos[i].time_to_eat = ft_atol(av[3]);
+        data->philos[i].time_to_sleep = ft_atol(av[4]);
+        data->philos[i].dead_lock = &data->dead_lock;
+        data->philos[i].meal_lock = &data->meal_lock;
+        data->philos[i].write_lock = &data->write_lock;
+        data->philos->r_fork = &data->forks[i];
+        data->philos->l_fork = &data->forks[(i + ft_atol(av[1] - 1) % ft_atol(av[1]))];
+        if (av[5])
+            data->philos->num_times_to_eat = ft_atol(av[5]);
+        else
+            data->philos->num_times_to_eat = -1;
+       ++i;
+    }
+}
+
+void *routine()
+{
+    printf("here\n");
+    return (0);
+}
+
+void creat_thread(t_main *data)
+{
+    int i;
+
+    i = 0;
+    while (i < data->philos->num_of_philos)
+    {
+        pthread_create(&data->philos[i].thread, NULL, &routine, NULL);
+        ++i;
+    }
+    i = 0;
+    while (i < data->philos->num_of_philos)
+    {
+        pthread_join(data->philos[i].thread, NULL);
+        ++i;
+    }
+    
+    
+}
 
 int main(int ac, char **av)
 {
-    t_data data;
+    t_main data;
 
     if (ac != 5 && ac != 6)
         exit_error("Wrong input\nEnter valid input ./philo 12 600 200 200 [5]");
     if (check_input(av, ac) == 1)
         exit_error("Error: All arguments must be positive digits.");
-        
-    
+    init_philo(&data, av);
+    creat_thread(&data);
+
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: mkhallou <mkhallou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 11:23:03 by mkhallou          #+#    #+#             */
-/*   Updated: 2025/06/30 19:55:28 by mkhallou         ###   ########.fr       */
+/*   Updated: 2025/07/03 14:39:08 by mkhallou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,38 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-typedef struct s_data	t_data;
-
-// typedef struct s_fork
-// {
-// 	pthread_mutex_t		mutex;
-// 	int					fork_id;
-// }						t_fork;
-
 typedef struct s_philo
 {
-	int					id;
-	pthread_t			thread_id;
-    pthread_mutex_t		*left_fork;
-    pthread_mutex_t		*right_fork;
-	long				last_meal_time;
-	int					count_meal;
-}						t_philo;
+	pthread_t		thread;
+	int				id;
+	int				eating;
+	int				meals_eaten;
+	size_t			last_meal;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				num_of_philos;
+	int				num_times_to_eat;
+	int				*dead;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+}					t_philo;
 
-
-struct					s_data
+typedef struct s_main
 {
-	int					nbr_philo;
-	long				time_die;
-	long				time_eat;
-	long				time_sleep;
-	long				must_eat;
-	t_philo				*philos;
-	pthread_mutex_t		*forks;
-	pthread_mutex_t		write;
-	pthread_mutex_t		stop;
-};
+	int				dead_flag;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	write_lock;
+	pthread_mutex_t	*forks;
+	t_philo			*philos;
+}					t_main;
 
-long					ft_atol(const char *str);
-int						ft_isdigit(int c);
-int						check_input(char **av, int ac);
-void					exit_error(char *str);
+long				ft_atol(const char *str);
+int					ft_isdigit(int c);
+int					check_input(char **av, int ac);
+void				exit_error(char *str);
