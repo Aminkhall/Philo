fb15cd18d394ea2d8c52a38de8a170275b766fd1
Mohamed Amine Khallouk <mkhallou@e2r7p4.1337.ma>
2025-07-09 12:27:38 +0100

    singl_th

--- philo/threads.c
+++ philo/threads.c
@@ -6,7 +6,7 @@
 /*   By: mkhallou <mkhallou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 15:30:14 by mkhallou          #+#    #+#             */
-/*   Updated: 2025/07/09 09:06:36 by mkhallou         ###   ########.fr       */
+/*   Updated: 2025/07/09 12:02:26 by mkhallou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void	handel_single_ph(t_philo *philo)
 {
+	pthread_mutex_lock(philo->r_fork);
 	my_print(philo, "taken a fork");
+	pthread_mutex_unlock(philo->r_fork);
 	return ;
 }
 
@@ -50,8 +52,7 @@ void	*monitor(void *args)
 		while (i < data->philos->num_of_philos)
 		{
 			if (get_current_time()
-				- (size_t)get_last_meal(&data->philos[i])
-				>= data->philos->time_to_die)
+				- (size_t)get_last_meal(&data->philos[i]) >= data->philos->time_to_die)
 			{
 				my_print(data->philos, "dead");
 				return (set_dead_flag(data, 1), NULL);
